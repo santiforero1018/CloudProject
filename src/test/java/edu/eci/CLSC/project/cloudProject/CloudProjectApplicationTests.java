24a6490706ea876ca4cd0edb115ca20ee3f24fd6
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2024-03-20 21:20:34 -0500

    estructura base del proyecto

---
+++ src/test/java/edu/eci/CLSC/project/cloudProject/CloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.CLSC.project.cloudProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
